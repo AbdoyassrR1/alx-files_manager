479d025d059ec61421449c54b4ecf3f988f59005
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-01 02:55:59 +0300

    UsersController

---
+++ controllers/UsersController.js
@@ -0,0 +1,53 @@
+#!/usr/bin/node
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
+import Queue from 'bull';
+
+const userQueue = new Queue('userQueue');
+
+
+class UsersController {
+  static async postNew(request, response) {
+    const { email, password } = request.body;
+
+    if (!email) {
+      return response.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) { 
+      return response.status(400).send({ error: 'Missing password' });
+    }
+
+    const emailExists = await dbClient.usersCollection.findOne({ email });
+    if (emailExists) {
+      return response.status(400).send({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+
+    let result;
+    try {
+      result = await dbClient.usersCollection.insertOne({
+        email,
+        password: hashedPassword,
+      });
+    } catch (err) {
+      await userQueue.add({});
+      return response.status(500).send({ error: 'Error creating user.' });
+    }
+
+    const user = {
+      id: result.insertedId,
+      email,
+    };
+
+    await userQueue.add({
+      userId: result.insertedId.toString(),
+    });
+
+    return response.status(201).send(user);
+
+  }
+}
+
+export default UsersController;
