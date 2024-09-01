0b16cb83f55be244ac88a0c5a98483c31f013739
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-01 03:40:33 +0300

    added functions for Basic Auth

---
+++ controllers/AuthController.js
@@ -0,0 +1,69 @@
+#!/usr/bin/node
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AuthController {
+  static async getConnect(request, response) {
+    const Authorization = request.header('Authorization') || '';
+
+    const credentials = Authorization.split(' ')[1];
+    if (!credentials) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const decodedCredentials = Buffer.from(credentials, 'base64').toString(
+      'utf-8',
+    );
+
+    const [email, password] = decodedCredentials.split(':');
+    if (!email || !password) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const hashedPassword = sha1(password);
+
+    const user = await dbClient.usersCollection.findOne({
+      email,
+      password: hashedPassword,
+    });
+    if (!user) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    const key = `auth_${token}`;
+    const hoursForExpiration = 24;
+
+    await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
+    return response.status(200).send({ token });
+  }
+
+  static async getDisconnect(request, response) {
+    const getUserIdAndKey = async (request) => {
+      const obj = { userId: null, key: null };
+
+      const xToken = request.header('X-Token');
+
+      if (!xToken) return obj;
+
+      obj.key = `auth_${xToken}`;
+
+      obj.userId = await redisClient.get(obj.key);
+
+      return obj;
+    };
+    const { userId, key } = await getUserIdAndKey(request);
+
+    if (!userId) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    await redisClient.del(key);
+
+    return response.status(204).send();
+  }
+}
+
+export default AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import Queue from 'bull';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const userQueue = new Queue('userQueue');
 
@@ -45,6 +47,36 @@ class UsersController {
     });
 
     return response.status(201).send(user);
+  }
+
+  static async getMe(request, response) {
+    const getUserIdAndKey = async (request) => {
+      const obj = { userId: null, key: null };
+
+      const xToken = request.header('X-Token');
+
+      if (!xToken) return obj;
+
+      obj.key = `auth_${xToken}`;
+
+      obj.userId = await redisClient.get(obj.key);
+
+      return obj;
+    };
+    const { userId } = await getUserIdAndKey(request);
+
+    const user = await dbClient.usersCollection.findOne({
+      _id: ObjectId(userId),
+    });
+    if (!user) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const processedUser = { id: user._id, ...user };
+    delete processedUser._id;
+    delete processedUser.password;
+
+    return response.status(200).send(processedUser);
   }
 }
 
