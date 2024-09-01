05c3134128b2be8181be071c32ce227000819d8f
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-01 17:00:07 +0300

    create an objID with new keyword

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,5 @@
-#!/usr/bin/node
-import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
+import sha1 from 'sha1';
 import Queue from 'bull';
 import dbClient from '../utils/db';
 import userUtils from '../utils/user';
@@ -11,26 +10,21 @@ class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
 
-    if (!email) {
-      return response.status(400).send({ error: 'Missing email' });
-    }
+    if (!email) return response.status(400).send({ error: 'Missing email' });
 
-    if (!password) {
-      return response.status(400).send({ error: 'Missing password' });
-    }
+    if (!password) { return response.status(400).send({ error: 'Missing password' }); }
 
     const emailExists = await dbClient.usersCollection.findOne({ email });
-    if (emailExists) {
-      return response.status(400).send({ error: 'Already exist' });
-    }
 
-    const hashedPassword = sha1(password);
+    if (emailExists) { return response.status(400).send({ error: 'Already exist' }); }
+
+    const sha1Password = sha1(password);
 
     let result;
     try {
       result = await dbClient.usersCollection.insertOne({
         email,
-        password: hashedPassword,
+        password: sha1Password,
       });
     } catch (err) {
       await userQueue.add({});
@@ -53,12 +47,10 @@ class UsersController {
     const { userId } = await userUtils.getUserIdAndKey(request);
 
     const user = await userUtils.getUser({
-      _id: ObjectId(userId),
+      _id: new ObjectId(userId),
     });
 
-    if (!user) {
-      return response.status(401).send({ error: 'Unauthorized' });
-    }
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
     const processedUser = { id: user._id, ...user };
     delete processedUser._id;
