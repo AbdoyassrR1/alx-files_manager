8d22d930b20d816f789683db68431fd0144daa04
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-01 16:48:36 +0300

    use the user utils instead

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,7 +3,7 @@ import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
 import Queue from 'bull';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import userUtils from '../utils/user';
 
 const userQueue = new Queue('userQueue');
 
@@ -50,26 +50,12 @@ class UsersController {
   }
 
   static async getMe(request, response) {
-    const getUserIdAndKey = async (request) => {
-      const obj = { userId: null, key: null };
+    const { userId } = await userUtils.getUserIdAndKey(request);
 
-      const xToken = request.header('X-Token');
-
-      if (!xToken) {
-        return obj;
-      }
-
-      obj.key = `auth_${xToken}`;
-
-      obj.userId = await redisClient.get(obj.key);
-
-      return obj;
-    };
-    const { userId } = await getUserIdAndKey(request);
-
-    const user = await dbClient.usersCollection.findOne({
+    const user = await userUtils.getUser({
       _id: ObjectId(userId),
     });
+
     if (!user) {
       return response.status(401).send({ error: 'Unauthorized' });
     }
